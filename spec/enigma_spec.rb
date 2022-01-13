04207ccc00ac961740d7499c6c39a6d6ce4dfc53
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-13 13:56:33 -0700

    feature: create Enigma class and pass exists test. Create .gitignore file and add /coverage/

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require './lib/enigma'
+
+RSpec.describe Enigma do
+  before(:each) do
+    @enigma = Enigma.new
+  end
+
+  it 'exists' do
+  expect(@enigma).to be_a(Enigma)
+  end
+end
