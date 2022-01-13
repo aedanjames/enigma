e7f412309a5972138c5e73a2afb237eef5026eab
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-13 14:07:39 -0700

    create tests for #encrypt and #decrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require './lib/enigma'
+require 'date'
 
 RSpec.describe Enigma do
   before(:each) do
@@ -8,5 +9,31 @@ RSpec.describe Enigma do
 
   it 'exists' do
   expect(@enigma).to be_a(Enigma)
+  end
+
+# encrypt a message with a key and a date
+  xit '#encrypt' do
+    actual = @enigma.encrypt("hello world", 02715, "040895")
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    expect(actual).to eq(expected)
+  end
+
+  xit '#decrypt' do
+    @enigma.encrypt("hello world", 02715, "040895")
+    actual = @enigma.decrypt("keder ohulw", "02715", "040895")
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    expect(actual).to eq(expected)
+  end
+
+  xit '#decrypt a message with a key (todays date)' do
+    
   end
 end
