f2ec77b1b342d553c8d009f38faaeed92044c7c7
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-15 19:47:03 -0700

    feature: CLI working, takes in data from txt files and outputs correctly

---
+++ lib/decrypt.rb
@@ -0,0 +1,15 @@
+require './lib/enigma'
+
+@enigma = Enigma.new
+
+input = ARGV[0]
+output = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+
+hash_output = @enigma.decrypt(File.read(input), key, date)
+
+File.open(output, "w") do |file|
+  file.puts hash_output[:decryption]
+  puts "created #{output} with key #{hash_output[:key]}, and date #{hash_output[:date]}"
+end
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,11 +1,14 @@
 require './lib/enigma'
 
-enigma = Enigma.new
-encryption = enigma.encrypt("Spicy mami hot Tamale", "54321")
+@enigma = Enigma.new
+
 input = ARGV[0]
 output = ARGV[1]
-ARGV[0] = File.read(input, chomp: true)
+
+incoming_text = File.new(input).read
+hash_output = @enigma.encrypt(incoming_text)
+
 File.open(output, "w") do |file|
-  file.puts encryption[:encryption]
-  puts "created #{output} with key #{encryption[:key]}, and date #{encryption[:date]}"
+  file.puts hash_output[:encryption]
+  puts "created #{output} with key #{hash_output[:key]}, and date #{hash_output[:date]}"
 end
